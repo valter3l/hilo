7b5a21d5d16dd3b71ef5e9d66a177caa21ed72d4
Fredy Navas <83843272+fredywnavas@users.noreply.github.com>
2022-05-20 14:21:55 -0500

    Updated methods documentation (method commentsw)
    As stated on the requirements: 
    (https://byui-cse.github.io/cse210-course-competency/abstraction/materials/hilo-specification.html)
    * The program must include class and method comments.

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -2,16 +2,28 @@ import random
 
 
 class Player:
+    """
+    Initializes the program
+    """
     def __init__(self):
         self.card_number = 0
         self.player = 0
         self.card_number = 0
         self.score = 300
 
+
     def get_card_number(self):
+        """
+        Represents cards from 1 to 13
+        """
         self.card_number = random.randint(1, 13)
 
+     
     def points_calculation(self):
+        """
+        Calculates the points in order to
+        continue with the game or game over
+        """        
         self.get_card_number()
         card_guess = input("Higher or lower? [h/l]: ")
         second_number = random.randint(1, 13)
